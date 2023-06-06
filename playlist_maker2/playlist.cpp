8aa98d9d82431cecc5abcc72f5e8e9e56d452c64
Samuel Rainey <131401403+samurai-98@users.noreply.github.com>
2023-06-06 11:52:23 -0600

    Update playlist.cpp

--- playlist_maker2/playlist.cpp
+++ playlist_maker2/playlist.cpp
@@ -12,17 +12,13 @@ void playlist::addSong(song title) {
     list[(list.size()-1)].setId(list.size());
 }
 
-//void playlist::removeSong(song title) {}
+void playlist::removeSong(song title) {}
 
 void playlist::printPlaylist() {
     unsigned int i;
-    //stringstream ss;
 
     for(i = 0; i < list.size(); i++) {
         cout << list[i].getId();
         cout << ": " << list[i].getArtist() << " - " << list[i].getName() << endl;
     }
-
-    //string songList = ss.str();
-    //return songList;
 }
