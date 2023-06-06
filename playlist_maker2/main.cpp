0b6a944cba1b6b1ac753005df3a8690a9765bf62
Samuel Rainey <131401403+samurai-98@users.noreply.github.com>
2023-06-06 11:50:33 -0600

    Update main.cpp

--- playlist_maker2/main.cpp
+++ playlist_maker2/main.cpp
@@ -8,48 +8,27 @@
 using namespace std;
 
 int main() {
-    //song playList[3];
-    //song obj;
     string songName;
     string artName;
-    /*unsigned int i;
-
-    for(i = 0; i < 3; i++){
-        cout << "Song:";
-        getline(cin, songName);
-        cout << endl;
-        cout << "Artist:";
-        getline(cin, artName);
-        cout << endl;
-
-        obj.setArtist(artName);
-        obj.setName(songName);
-        obj.setId((i+1));
-        playList[i] = obj;
-    }
-
-    for(i = 0; i < 3; i++) {
-        cout << playList[i].getId();
-        cout << ": " << playList[i].getArtist() << " - " << playList[i].getName() << endl;
-    }*/
-    //int num;
+    int num;
     int j;
     playlist myList;
 
-    /*cout << "How many songs would you like to add?: ";
-    cin >> num;*/
-    for (j = 0; j < 3; j++) {
+    cout << "How many songs would you like to add?: ";
+    cin >> num;
+    int listLen = num;
+    cin.ignore();
+    for (j = 0; j < num; j++) {
         song obj;
-        cout << "Name:";
+        cout << "Name: ";
         getline(cin, songName);
         cout << endl;
-        cout << "Artist:";
+        cout << "Artist: ";
         getline(cin, artName);
         cout << endl;
 
         obj.setArtist(artName);
         obj.setName(songName);
-        //obj.setId((j+1));
         myList.addSong(obj);
     }
 
