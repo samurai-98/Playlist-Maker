51b76038f2dd5a26ea10610550489b8c178f855d
Samuel Rainey <131401403+samurai-98@users.noreply.github.com>
2023-06-13 15:10:19 -0600

    Update playlist.cpp

--- playlist_maker2/playlist.cpp
+++ playlist_maker2/playlist.cpp
@@ -2,23 +2,75 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <map>
 #include "playlist.h"
+#include "song.h"
 
 using namespace std;
 
-    
+/*string playlist::lowerCase(string line) {
+    transform(line.begin(), line.end(), line.begin(), [](unsigned char c){ return tolower(c); });
+    return line;
+}*/
+
 void playlist::addSong(song title) {
-    list.push_back(title);
-    list[(list.size()-1)].setId(list.size());
+    string player = title.getArtist();
+    string songName = title.getName();
+
+    if (songMap.find(player) == songMap.end()) {
+        vector<string> tmpVec;
+        tmpVec.push_back(songName);
+        songMap[player] = tmpVec;
+    } else {
+        songMap[player].push_back(songName);
+    }
+    
 }
 
-void playlist::removeSong(song title) {}
+void playlist::removeSong() {
+    string player;
+    cin.ignore();
+    cout << "Artist: ";
+    getline(cin, player);
+    cout << endl;
+
+    string songName;
+    cout << "Name: ";
+    getline(cin, songName);
+    cout << endl;
+
+    int size = songMap[player].size();
+    vector<string> tmpVec;
+    int i;
+
+    for (i = 0; i < size; i++) {
+        if (songMap[player][i] != songName) {
+            tmpVec.push_back(songMap[player][i]);
+        }
+    }
+    songMap[player].clear();
+    songMap[player] = tmpVec;
+}
+
+string playlist::printVec(vector<string> strVec) {
+    int i;
+    stringstream ss;
+    int size = strVec.size();
+    for (i = 0; i < strVec.size(); i++) {
+        if (i != (size - 1)) {
+            ss << strVec[i] << ", ";
+        } else {
+            ss << strVec[i];
+        }
+    }
+    string res = ss.str();
+    return res;
+}
 
 void playlist::printPlaylist() {
-    unsigned int i;
-
-    for(i = 0; i < list.size(); i++) {
-        cout << list[i].getId();
-        cout << ": " << list[i].getArtist() << " - " << list[i].getName() << endl;
+    for (auto pair: songMap) {
+        cout << pair.first << " - " << printVec(pair.second) << endl;
     }
 }
